7d76360c78d42b75641e9246265ce6ae1bdb1aa9
Romain Forlot <rforlot@yahoo.com>
2012-08-08 23:07:49 +0200

    Add script to handle incron action (blacklist vim temp work file etc)

---
+++ incron_action.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DIR=$1
+FILE=$2
+
+EXT=$(echo $2 | awk -F '.' '{print $NF}')
+
+BACKUPDIR=/data/backup
+GITBIN=$(which git)
+LOGGERBIN=$(which logger)
+
+function backupthat
+{
+    ${LOGGERBIN} -p local0.info -t incron_action "Incron Autocommit, file ${DIR}/${FILE} has been modified."
+    cd $DIR
+    ${GITBIN} commit -q -a -m "Incron Autocommit, file ${DIR}/${FILE} has been modified"
+    cd ${BACKUPDIR}${DIR}
+    ${GITBIN} pull
+}
+
+case $EXT in
+    swp|swx|4913)
+        echo $FILE
+        exit 0
+        ;;
+    *)
+		case $DIR in
+            /etc*)
+                backupthat
+                ;;
+		    /var/named)
+		        /usr/bin/systemctl reload named.service
+                backupthat
+		        ;;
+		    /var/kerberos)
+		        /usr/bin/systemctl reload krb5kdc.service 
+                backupthat
+		        ;;
+		    /var/www|/var/lib*)
+                backupthat
+		        ;;
+        esac
+        ;;
+esac
