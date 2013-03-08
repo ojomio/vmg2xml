7aa74c83ca019eb6bf2cdd1444d7597224b3c282
Romain Forlot <rforlot@yahoo.com>
2013-03-08 15:19:18 +0100

    Enh incron_action: Fix infinite loop reloading service and had lock file

--- incron_action.sh
+++ incron_action.sh
@@ -5,22 +5,54 @@ FILE=$2
 
 EXT=$(echo $2 | awk -F '.' '{print $NF}')
 
-BACKUPDIR=/data/backup
+trap unlock SIGHUP SIGINT SIGTERM SIGKILL
+
+RUNDIR=/run
+BACKUPDIR=/backup
 GITBIN=$(which git)
+DATEBIN=$(which date)
 LOGGERBIN=$(which logger)
+SYSTEMCTLBIN=$(which systemctl)
+
+function unlock()
+{
+    if [ -n $1 ]
+    then
+        rm -f $1
+    fi
+}
+
+function reload_service()
+{
+    StartDate=$($SYSTEMCTLBIN show --property=ExecMainStartTimestamp $1 | cut -d '=' -f2)
+    StartTimestamp=$(date -d '$StartDate' +%s)
+    ActualTimestamp=$($DATEBIN +%s)
+    DeltaTimestamp=$(echo "${ActualTimestamp}-${StartTimestamp}" | bc)
+    if [ $DeltaTimestamp -gt 1 ]
+    then
+        $SYSTEMCTLBIN reload $1
+    fi
+}
 
 function backupthat
 {
-    ${LOGGERBIN} -p local0.info -t incron_action "Incron Autocommit, file ${DIR}/${FILE} has been modified."
+    # Create lock for the dir git commited and pulled
+    Simple_Dir=$(echo "$DIR" | sed 's/\//_/g')
+    LockFile=${RUNDIR}/incron$Simple_Dir.lock
+    [ -f $LockFile ] && exit 1
+    touch $LockFile
+
+    ${LOGGERBIN} -p local0.info -t incron_action "Incron Autocommit, file ${FILE} has been modified."
     cd $DIR
-    ${GITBIN} commit -q -a -m "Incron Autocommit, file ${DIR}/${FILE} has been modified"
+    ${GITBIN} commit -a -m "Incron Autocommit, file ${FILE} has been modified" | grep -vE "^#"
     cd ${BACKUPDIR}${DIR}
     ${GITBIN} pull
+    unlock ${LockFile}
 }
 
-case $EXT in
-    *ld.so.cache*|swp|swx|4913|*~)
-        echo $FILE
+case $FILE in
+    *ld.so.cache*|*.swp|*.swx|4913|*~)
+        ${LOGGERBIN} -p local0.info -t incron_action "Incron denied file ${FILE}"
         exit 0
         ;;
     *)
@@ -28,15 +60,15 @@ case $EXT in
             /etc*)
                 backupthat
                 ;;
-		    /var/named)
-		        /usr/bin/systemctl reload named.service
+		    /var/named*)
+                reload_service named
                 backupthat
 		        ;;
-		    /var/kerberos)
-		        /usr/bin/systemctl reload krb5kdc.service 
+		    /var/kerberos*)
+                reload_service krb5kdc
                 backupthat
 		        ;;
-		    /var/www|/var/lib*)
+		    /var/www*|/var/lib*)
                 backupthat
 		        ;;
         esac
